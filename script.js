098b5a49853fe2e467c1f91b1a2f71c30b76e61a
chiragmhatre <chiragmhatre128@gmail.com>
2024-04-17 06:22:38 +0530

    first push

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('sendFlowers').addEventListener('click', function() {
+    // This is where you can add logic to send virtual flowers
+    alert('Virtual flowers sent with a personalized note!');
+});
+
+// You can add JavaScript code here for animations or interactive elements
